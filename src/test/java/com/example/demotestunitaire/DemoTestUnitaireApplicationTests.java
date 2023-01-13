9e3596dfda880ad405ebd85ba8d5b5528fa1dc6c
Vincent Ndouma <vincentndong477@gmail.com>
2023-01-13 12:35:02 +0100

    premier commit second projet maven

---
+++ src/test/java/com/example/demotestunitaire/DemoTestUnitaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotestunitaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestUnitaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
